6b7251b15a4e7080e473473fd7216e39fb2c03e5
Tchun Chao <chaot_xp@live.fr>
2018-02-07 09:54:18 -0500

    Début de l'exercice 4

---
+++ Exercice4.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+////////////////////////////////////// Route /html/01_form.html
+app.get('/html/02_form.html', function (req, res) {
+ console.log(__dirname);
+ res.sendFile( __dirname + "/" + "02_form.html" );
+})
+
+////////////////////////////////////// Route /
+app.get('/', (req, res) => {
+ console.log('accueil')
+ res.end('<h1>Accueil</h1>')
+})
+
+////////////////////////////////////// Route /traiter_get
+app.get('/traiter_get', function (req, res) {
+ // Preparer l'output en format JSON
+
+console.log('la route /traiter_get')
+
+// on utilise l'objet req.query pour récupérer les données GET
+ let reponse = {
+ prenom:req.query.prenom,
+ nom:req.query.nom,
+ tel:req.query.tel,
+ courriel:req.query.courriel
+ };
+console.log(reponse);
+ res.end(JSON.stringify(reponse));
+})
+
+var server = app.listen(8081, function () {
+ var host = server.address().address
+ var port = server.address().port
+ 
+ console.log("Exemple l'application écoute sur http://%s:%s", host, port)
+
+})
